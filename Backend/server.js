a32e3fb191f658a933828e7398890d3d99ff7b15
Arcane-Ayush <ayush2017iu@gmail.com>
2025-12-04 21:53:21 +0530

    Ah.. missed the modules

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+// import express from 'express'
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const PORT = 3000;
+
+const FRONTEND_PATH = path.join(__dirname , '..' , 'Frontend');
+app.use(express.static(FRONTEND_PATH)); //__dirname is inbuilt node module 
+
+app.get('/' , (req , res) => {
+    res.sendFile(path.join(__dirname , 'Frontend' , 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`YAY!! server is running on port:${PORT}\n
+http://localhost:3000`);
+})
